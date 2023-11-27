42f93ddf32f4632373701055d64ef24cdab3b3f8
muathye <muath.ye@gmail.com>
2023-11-27 14:45:38 +0300

    chore: update comments migration table

--- database/migrations/2023_11_27_114206_create_comments_table.php
+++ database/migrations/2023_11_27_114206_create_comments_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,8 +14,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create((new Comment())->getTable(), function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->restrictOnDelete();
+            $table->foreignIdFor(Post::class)->constrained()->restrictOnDelete();
+            $table->longText('body');
             $table->timestamps();
         });
     }
