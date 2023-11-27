41414fe612c0500a0f74b5b5dce818bbf6547ce5
muathye <muath.ye@gmail.com>
2023-11-28 00:09:20 +0300

    feat: update posts page with AppLayout
    AppLayout now includes the links depending on auth status
    Index of Posts uses the AppLayout

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,1 +1,8 @@
-<template></template>
+<template>
+    <AppLayout title="Posts">
+        Hello world
+    </AppLayout>
+</template>
+<script setup>
+    import AppLayout from "@/Layouts/AppLayout.vue"
+</script>
