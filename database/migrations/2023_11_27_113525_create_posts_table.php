46bda6cfd81d043319807284b8df1ae43e109432
muathye <muath.ye@gmail.com>
2023-11-27 14:41:39 +0300

    chore: update posts migration table

--- database/migrations/2023_11_27_113525_create_posts_table.php
+++ database/migrations/2023_11_27_113525_create_posts_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,8 +13,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create((new Post())->getTable(), function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->restrictOnDelete();
+            $table->string('title');
+            $table->longText('body');
             $table->timestamps();
         });
     }
