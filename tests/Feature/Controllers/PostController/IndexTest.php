8120e8c2356a9d2bbab86bf40c715f2928dc9b66
muathye <muath.ye@gmail.com>
2023-12-06 10:32:58 +0300

    feat: add assertHasResource macro for TestResponse

--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -2,6 +2,8 @@
 
 use App\Http\Resources\PostResource;
 use App\Models\Post;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Testing\TestResponse;
 use Inertia\Testing\AssertableInertia;
 
 use function Pest\Laravel\get;
@@ -14,15 +16,18 @@ it('should return the correct component', function () {
 });
 
 it('passes posts to the view', function () {
-
-
     // I've create as much data as required for the test, and no more.
     // Because it takes time to actually build those models...
     $posts = Post::factory(3)->create();
 
+    TestResponse::macro('assertHasResource', function (string $key, JsonResource $resource) {
+        return $this->assertInertia(fn (AssertableInertia $inertia) => $inertia->hasResource($key, $resource));
+    });
+
     get(route('posts.index'))
+        ->assertHasResource('post', PostResource::make($posts->first()))
         ->assertInertia(fn (AssertableInertia $inertia) => $inertia
-            ->hasResource('post', PostResource::make($posts->first()))
+            // ->hasResource('post', PostResource::make($posts->first()))
             ->hasPaginatedResource('posts', PostResource::collection($posts->reverse()))
         );
 });
