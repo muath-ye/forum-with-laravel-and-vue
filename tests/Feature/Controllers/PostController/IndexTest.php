70f4306116313c2d273fe683d350399f04971ee4
muathye <34031333+muath-ye@users.noreply.github.com>
2024-07-18 11:30:57 +0300

    chore: using assertComponent macro on PostController/IndexTest

--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -8,9 +8,7 @@ use function Pest\Laravel\get;
 
 it('should return the correct component', function () {
     get(route('posts.index'))
-        ->assertInertia(fn (AssertableInertia $inertia) => $inertia
-            ->component('Posts/Index', true)
-        );
+        ->assertComponent('Posts/Index', true);
 });
 
 it('passes posts to the view', function () {
