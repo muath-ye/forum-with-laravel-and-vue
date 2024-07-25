44acc5f8f56d10de7fb3b1fb74a90679cf9f8850
muathye <34031333+muath-ye@users.noreply.github.com>
2024-07-25 14:13:06 +0300

    feat: show user name with formated date of the post

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -4,7 +4,8 @@
             <ul class="divide-y">
                 <li v-for="post in posts.data" :key="post.id" class="px-2 py-4">
                     <Link :href="route('posts.show', post.id)">
-                    <span class="text-bold text-lg">{{ post.title }}</span>
+                    <span class="text-bold text-lg hover:text-blue-500">{{ post.title }}</span>
+                    <span class="block mt-1 text-sm text-gray-600">{{ formattedDate(post) }} ago by {{ post.user.name }}</span>
                     </Link>
                 </li>
             </ul>
@@ -18,6 +19,11 @@ import AppLayout from "@/Layouts/AppLayout.vue"
 import Container from "@/Components/Container.vue"
 import Pagination from "@/Components/Pagination.vue"
 import { Link } from "@inertiajs/vue3"
+import { formatDistance, parseISO } from "date-fns"
 
-defineProps(['posts']);
+const props = defineProps(['posts']);
+console.log(props.posts)
+const formattedDate = (post) => {
+    return formatDistance(parseISO(post.created_at), new Date());
+}
 </script>
