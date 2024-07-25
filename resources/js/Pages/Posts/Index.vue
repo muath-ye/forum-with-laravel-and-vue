aed35eb2c5c91989ea9f02b72d404e8f6f9192a9
muathye <34031333+muath-ye@users.noreply.github.com>
2024-07-25 12:18:50 +0300

    feat: add link to post titles on posts index page

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -3,7 +3,9 @@
         <Container>
             <ul class="divide-y">
                 <li v-for="post in posts.data" :key="post.id" class="px-2 py-4">
+                    <Link :href="route('posts.show', post.id)">
                     <span class="text-bold text-lg">{{ post.title }}</span>
+                    </Link>
                 </li>
             </ul>
 
@@ -15,6 +17,7 @@
 import AppLayout from "@/Layouts/AppLayout.vue"
 import Container from "@/Components/Container.vue"
 import Pagination from "@/Components/Pagination.vue"
+import { Link } from "@inertiajs/vue3"
 
 defineProps(['posts']);
 </script>
