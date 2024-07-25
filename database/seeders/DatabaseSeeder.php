5708e5f6e7730d6e222fedf3f54556a2ce0453d0
muathye <34031333+muath-ye@users.noreply.github.com>
2024-07-25 16:34:25 +0300

    chore: update Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         $users = User::factory(10)->create();
 
-        $posts = Post::factory(200)->recycle($users)->create();
-
-        $comments = Comment::factory(10)->recycle($users)->recycle($posts)->create();
+        $posts = Post::factory(200)->has(Comment::factory(15)->recycle($users))->recycle($users)->create();
 
         $muath = User::factory()
             ->has(Post::factory(45))
