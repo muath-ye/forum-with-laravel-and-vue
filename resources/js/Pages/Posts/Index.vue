63f1586a580b70b49a842185a0b7ec924fb32241
muathye <34031333+muath-ye@users.noreply.github.com>
2024-07-25 14:14:46 +0300

    chore: update the color of the hover

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -4,7 +4,7 @@
             <ul class="divide-y">
                 <li v-for="post in posts.data" :key="post.id" class="px-2 py-4">
                     <Link :href="route('posts.show', post.id)" class="group">
-                    <span class="text-bold text-lg group-hover:text-blue-500">{{ post.title }}</span>
+                    <span class="text-bold text-lg group-hover:text-indigo-500">{{ post.title }}</span>
                     <span class="block mt-1 text-sm text-gray-600">{{ formattedDate(post) }} ago by {{ post.user.name }}</span>
                     </Link>
                 </li>
