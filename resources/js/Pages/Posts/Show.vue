8d868003e88771c9dc5050f813c400f37a368e97
muathye <34031333+muath-ye@users.noreply.github.com>
2024-07-25 16:38:28 +0300

    chore: add comments to show post page

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -6,16 +6,28 @@
             <article class="mt-6">
                 <pre class="font-sans whitespace-pre-wrap">{{ post.body }}</pre>
             </article>
+            <div class="mt-12">
+                <h2 class="text-xl font-semibold">Comments</h2>
+                <ul class="divide-y mt-4">
+                    <li v-for="comment in comments.data" :key="comment.id" class="px-2 py-4">
+                        <Comment :comment="comment" />
+                    </li>
+                </ul>
+                <Pagination :meta="comments.meta"/>
+            </div>
         </Container>
     </AppLayout>
 </template>
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue"
 import Container from "@/Components/Container.vue"
+import Comment from "@/Components/Comment.vue"
 import { formatDistance, parseISO } from "date-fns"
 import { computed } from "vue"
+import Pagination from "@/Components/Pagination.vue";
+import { relativeDate } from "@/Utilities/date";
 
-const props = defineProps(['post']);
+const props = defineProps(['post', 'comments']);
 
-const formattedDate = computed(() => formatDistance(parseISO(props.post.created_at), new Date()));
+const formattedDate = computed(() => relativeDate(props.post.created_at));
 </script>
